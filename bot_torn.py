575094d25035ca2d3006dd79e2372771f5af09dc
LFFcoding <leofreitapro@yahoo.com.br>
2023-08-16 07:53:20 -0300

    added a longer time between executions and loop improvements

--- bot_torn.py
+++ bot_torn.py
@@ -10,7 +10,6 @@ import json
 with open("./selectors.json") as file:
     selectors = json.load(file)
 
-#rodando = True
 crimes = 0
 treinos = 0
 
@@ -22,11 +21,24 @@ options.add_argument('--user-data-dir=c:/user')
 
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 
+#função para pausa aleatória, simulando ação humana
+#def randompause():
+#    x = random.randint(20, 40) * 60
+#    print("Pause " + str(x / 60) + " minutes. " + tmst())
+#    time.sleep(x)
+
+#func para coletar os valores das bars
+def barvalues():
+    energy = int(driver.find_element(By.CSS_SELECTOR, selectors["energy"]).text.split("/")[0])
+    nerve = int(driver.find_element(By.CSS_SELECTOR, selectors["nerve"]).text.split("/")[0])
+    return energy, nerve
+
 # func para reconhecer os status
-def inter(n):
-   nbr = n.split("/")
-   nbr = int(nbr[0])
-   return nbr
+def stats():
+    strengh = driver.find_element(By.CSS_SELECTOR, selectors["strval"]).text
+    speed = driver.find_element(By.CSS_SELECTOR, selectors["spdval"]).text
+    defence = driver.find_element(By.CSS_SELECTOR, selectors["defval"]).text
+    print("Stats: Força: " + strengh + " | Velocidade: " + speed + " | Defesa: " + defence + " | " + tmst())
 
 #func para ir para a página de treino e clicar no botão
 def treinar():
@@ -35,28 +47,27 @@ def treinar():
         train = WebDriverWait(driver, 20).until(
                 EC.visibility_of_element_located((By.CSS_SELECTOR, selectors["spdbutton"]))
             )
+        time.sleep(2)
         train.click()
-#        botao = driver.find_elements(By.XPATH, "/html/body/div[5]/div/div[2]/div[4]/div/div[2]/div/ul/li[1]/div[2]/div[2]/button")
-#        botao[0].click()
+        time.sleep(2)
     except Exception as e:
         print(f"Ocorreu um erro no treino: {e}" + tmst())
     finally:
-        print("Treinou" + tmst())
+        print("Treinou! " + tmst())
+        stats()
 
 #func para consumir nerve
 def commitCrime():
     try:
         driver.get(selectors["crimescreen"])
         slradio = WebDriverWait(driver, 20).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, selectors["crimeshoplift"]))
+                EC.visibility_of_element_located((By.CSS_SELECTOR, selectors["crimesfc"]))
             )
+        time.sleep(2)
         slradio.click()
         time.sleep(5)
         driver.find_element(By.CSS_SELECTOR, selectors["docrime"]).click()
         time.sleep(5)
-        driver.find_element(By.CSS_SELECTOR, selectors["docrime"]).click()
-#        botao = driver.find_elements(By.XPATH, "/html/body/div[5]/div/div[2]/div[4]/div/div[2]/div/ul/li[1]/div[2]/div[2]/button")
-#        botao[0].click()
     except Exception as e:
         print(f"Ocorreu um erro no crime: {e}" + tmst())
     finally:
@@ -71,53 +82,37 @@ def tmst():
 
 #execução principal
 while(True):
-    try:
-        driver.get(selectors["bigboxlink"])
-        print(f"Início: " + tmst())
-        while(True):
-            energy = WebDriverWait(driver, 20).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, selectors["energy"]))
-            )
 
-            energy = inter(energy.text)
-            nerve = inter(driver.find_element(By.CSS_SELECTOR, selectors["nerve"]).text)
+    try:
+
+        driver.get(selectors["bigboxlink"])
+
+        time.sleep(5)
+
+        print(f"Início: " + tmst())
+
+        while(True):
+
+            energy, nerve = barvalues()
             print(f"Energy: " + str(energy) + " | " + "Nerve: " + str(nerve) + " " + tmst())
-            if energy >= 5:
+
+            while (energy >= 5):
                 treinar()
                 treinos += 1
                 print(f"Treinos: " + str(treinos))
-            elif nerve >= 4:
+                energy, nerve = barvalues()
+            while (nerve >= 2):
                 commitCrime()
                 crimes += 1
                 print(f"Crimes: " + str(crimes))
-            print("Pause 5 minutes.")
-            time.sleep(300)
-                
+                energy, nerve = barvalues()
+            print("Pause 40 minutes. " + tmst())
+            time.sleep(2400)
 
-
-#               elementos = driver.find_elements(By.CLASS_NAME, "bar-value___NTdce")
-#   
-#               textos = []
-#   
-#               for elemento in elementos:
-#                   textos.append(elemento.text)
-#   
-#               energy = inter(textos[0])
-#   
-#               if(energy >= 6):
-#                   print(f"Cheio de energia, bora treinar! " + tmst())
-#                   treinar()
-#                   forcaatl = driver.find_element(By.ID, "strength-val").text
-#                   treinos += 1
-#               else:
-#                   print("Calma que tu tá exausto! Descanso de 5 minutos " + tmst())
-#                   time.sleep(300)
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
         print(f"Aguardando 30 segundos para reiniciar... " + tmst())
         time.sleep(30)
-#           print("Reiniciando. " + tmst())
-#           driver.get("https://www.torn.com/index.php")
 
     finally:
-        time.sleep(900)
+        time.sleep(300)
